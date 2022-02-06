48e4c76533d3a259194f6cb206555b7cf342a285
Administrator <godxuche@gmail.com>
2022-02-06 23:07:34 +0100

    set up the navigation component and created the gallery fragment...

---
+++ app/src/main/java/com/godzuche/unsplashimageapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package com.godzuche.unsplashimageapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.godzuche.unsplashimageapp.R
+
+class GalleryFragment: Fragment(R.layout.fragment_gallery) {
+}
