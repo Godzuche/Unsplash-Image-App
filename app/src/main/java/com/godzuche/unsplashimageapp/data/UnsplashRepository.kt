f8a52b03f5f4ab02a42c49c5c342fa621e6606ee
Administrator <godxuche@gmail.com>
2022-02-10 17:15:04 +0100

    set up the repository with the Pager to return a flow of paging data...

--- app/src/main/java/com/godzuche/unsplashimageapp/data/UnsplashRepository.kt
+++ app/src/main/java/com/godzuche/unsplashimageapp/data/UnsplashRepository.kt
@@ -1,11 +1,36 @@
 package com.godzuche.unsplashimageapp.data
 
+import android.util.Log
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.godzuche.unsplashimageapp.api.UnsplashApiService
+import com.godzuche.unsplashimageapp.data.remote.model.UnsplashPhoto
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UnsplashRepository @Inject constructor(
-    private val unsplashApi: UnsplashApiService
+    private val unsplashApi: UnsplashApiService,
 ) {
+
+    // Get search results as a stream using flow
+    fun getSearchResultStream(query: String): Flow<PagingData<UnsplashPhoto>> {
+        Log.d("UnsplashRepository", "New query: $query")
+        return Pager(
+            config = PagingConfig(
+                pageSize = NETWORK_PAGE_SIZE,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {
+                UnsplashPagingSource(unsplashApi, query)
+            }
+        ).flow
+    }
+
+    companion object {
+        const val NETWORK_PAGE_SIZE = 20
+    }
 }
