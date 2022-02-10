d18323aea9c84fabc6a16428f52940c0f55b5de8
Administrator <godxuche@gmail.com>
2022-02-10 20:24:49 +0100

    collect the flow of paging data from the viewModel...

--- app/src/main/java/com/godzuche/unsplashimageapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/godzuche/unsplashimageapp/ui/gallery/GalleryFragment.kt
@@ -1,9 +1,32 @@
 package com.godzuche.unsplashimageapp.ui.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.godzuche.unsplashimageapp.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@ExperimentalCoroutinesApi
 @AndroidEntryPoint
 class GalleryFragment: Fragment(R.layout.fragment_gallery) {
+    private val viewModel by viewModels<GalleryViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.photosPagingDataFlow.collectLatest {
+
+                }
+            }
+        }
+    }
 }
