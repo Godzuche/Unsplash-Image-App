830f0d907935813b6d5f56033d1152ee0a2198a0
Administrator <godxuche@gmail.com>
2022-02-12 23:59:03 +0100

    collected the paging data and set up the header and footer to the adapter...

--- app/src/main/java/com/godzuche/unsplashimageapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/godzuche/unsplashimageapp/ui/gallery/GalleryFragment.kt
@@ -8,6 +8,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.godzuche.unsplashimageapp.R
+import com.godzuche.unsplashimageapp.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collectLatest
@@ -15,18 +16,37 @@ import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 @AndroidEntryPoint
-class GalleryFragment: Fragment(R.layout.fragment_gallery) {
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
     private val viewModel by viewModels<GalleryViewModel>()
+    private var _binding: FragmentGalleryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.photosPagingDataFlow.collectLatest {
+        _binding = FragmentGalleryBinding.bind(view)
 
-                }
+        val unsplashPhotoAdapter = UnsplashPhotoAdapter()
+
+        binding.apply {
+            recyclerview.apply {
+                setHasFixedSize(true)
+                adapter = unsplashPhotoAdapter.withLoadStateHeaderAndFooter(
+                    header = UnsplashPhotoLoadStateAdapter { unsplashPhotoAdapter.retry() },
+                    footer = UnsplashPhotoLoadStateAdapter { unsplashPhotoAdapter.retry() }
+                )
             }
         }
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.photosPagingDataFlow.collectLatest(unsplashPhotoAdapter::submitData)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
