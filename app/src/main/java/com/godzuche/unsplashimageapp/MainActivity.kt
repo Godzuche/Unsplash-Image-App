e5be965ad32be3e4b8fffbf3b184f1623a23a52e
Administrator <godxuche@gmail.com>
2022-02-13 11:28:03 +0100

    added and set up toolbar that hides on scroll...

--- app/src/main/java/com/godzuche/unsplashimageapp/MainActivity.kt
+++ app/src/main/java/com/godzuche/unsplashimageapp/MainActivity.kt
@@ -2,12 +2,40 @@ package com.godzuche.unsplashimageapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.godzuche.unsplashimageapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.apply {
+            setSupportActionBar(toolbarMain)
+            setupActionBarWithNavController(navController)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
